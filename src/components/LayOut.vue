79767938dde702913e138731099bafedfe082677
aliyu <1324149139@qq.com>
2022-04-06 16:11:01 +0800

    Fix bugs

--- src/components/LayOut.vue
+++ src/components/LayOut.vue
@@ -8,9 +8,6 @@
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/money.svg';
-
-console.log(x);
 export default {
   name: "LayOut"
 };
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#label"></use>
+      </svg>
+      记账
+    </router-link>
     <router-link to="/labels">标签</router-link>
     <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
+import x from '@/assets/icons/label.svg';
+
+console.log(x);
 export default {
   name: "Nav"
 };
