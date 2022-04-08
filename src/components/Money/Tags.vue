c441ed9a882e689ab055dc311c33ec1a9b08206f
aliyu <1324149139@qq.com>
2022-04-08 18:51:31 +0800

    Adjust the style

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,7 +4,11 @@
       <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag" @click="toggle(tag)" :class="{selected:selectedTags.indexOf(tag)>=0}">{{tag}}</li>
+      <li v-for="tag in dataSource" :key="tag"
+          @click="toggle(tag)"
+          :class="{selected:selectedTags.indexOf(tag)>=0}">
+           {{tag}}
+      </li>
     </ul>
   </div>
 
