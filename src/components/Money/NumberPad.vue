ac89b2d6d9c7c56c38303dd6f85eb64eadbffe27
aliyu <1324149139@qq.com>
2022-04-11 17:39:35 +0800

    Persisting LocalStorage and adding creation time fields

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -62,6 +62,7 @@ export default class NumberPad extends Vue {
 
   ok(){
     this.$emit('update:value',this.output);
+    this.$emit('submit',this.output);
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <LayOut class-prefix="layout">
-    {{ record }}
-    <NumberPad @update:value="onUpdateAmount"/>
+    {{ recordList }}
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,19 +14,22 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
-import {Component} from "vue-property-decorator";
+import {Component, Watch} from "vue-property-decorator";
 
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
 type Record = {
   tags: string[]
   notes: string
   type: string
   amount: number
+  createAt?:Date
 }
 @Component({
   components: {Tags, Notes, Types, NumberPad},
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
+  recordList: Record[] = recordList;
   record: Record = {
     tags: [], notes: "", type: "-", amount: 0
   };
@@ -41,6 +44,17 @@ export default class Money extends Vue {
 
   onUpdateAmount(value: string) {
     this.record.amount = parseFloat(value);
+  }
+
+  saveRecord(){
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    // add time
+    record2.createAt = new Date();
+    this.recordList.push(record2);
+  }
+  @Watch('recordList')
+  onRecordListChange(){
+    window.localStorage.setItem('recordList',JSON.stringify(this.recordList));
   }
 }
 </script>
