e818dafee7ef4ca4d1ad74aeaa3228a1cccc9652
aliyu <1324149139@qq.com>
2022-04-11 20:41:17 +0800

    add right.svg

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
-    <LayOut>
-      <p>Labels</p>
-    </LayOut>
-  </div>
+  <LayOut>
+    <ul class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ul>
+    <div>
+      <button>新建标签</button>
+    </div>
+  </LayOut>
 </template>
 
 <script lang="ts">
@@ -11,3 +29,18 @@ export default {
   name: "Labels"
 };
 </script>
+
+<style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+  }
+}
+</style>
