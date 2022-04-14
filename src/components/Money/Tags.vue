5ad4bdbd1076792bda8000705e41cb8f0424384b
aliyu <1324149139@qq.com>
2022-04-14 08:28:44 +0800

    重新封装 recordListModels

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -35,10 +35,9 @@ export default class Tags extends Vue {
     const  name = window.prompt('请输入标签名');
     if (name === ''){
         window.alert('标签名不能为空');
-    }else {
-       if (this.dataSource){
+    }else if (this.dataSource) {
+         console.log(this.dataSource);
          this.$emit('update:dataSource',[...this.dataSource,name]);
-       }
     }
   }
 }
--- src/models/recordListModels.ts
+++ src/models/recordListModels.ts
@@ -1,13 +1,15 @@
-const localStorageKeyName = 'recordList';
+const localStorageKeyName = "recordList";
 const recordListModel = {
-    clone(data: RecordItem[] | RecordItem){
+    data: [] as RecordItem[],
+    clone(data: RecordItem[] | RecordItem) {
         return JSON.parse(JSON.stringify(data));
     },
-    fetch(){
-        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    fetch() {
+        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") as RecordItem[];
+        return this.data;
     },
-    save(data: RecordItem[]){
-        window.localStorage.setItem(localStorageKeyName,JSON.stringify(data));
+    save() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
     }
-}
+};
 export default recordListModel;
