a5247fe0436a5525f459b276e53661fe1b0e87f6
aliyu <1324149139@qq.com>
2022-04-14 18:04:16 +0800

    解决git提交冲突

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -2,11 +2,10 @@ import recordStore from "@/store/recordStore";
 import tagStore from "@/store/tagStore";
 
 const store = {
-    // record store
     ...recordStore,
-    // tag store
-    ...tagStore
-
+    ...tagStore,
 };
+
+console.log(store);
 
 export default store;
