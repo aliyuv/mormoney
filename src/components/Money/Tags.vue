47bcd219c3d7ba1a8b75b1c1e8c1030d63093090
aliyu <1324149139@qq.com>
2022-04-15 18:02:36 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,15 +19,16 @@ import {Component} from 'vue-property-decorator';
 @Component({
   computed:{
     tagList(){
-      //TODO
-    // return this.$store.fetchTags();
-      return [];
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
-
   selectedTags: string[] = [];
+
+  created(){
+    this.$store.commit('fetchTags');
+  }
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
@@ -40,8 +41,7 @@ export default class Tags extends Vue {
   create() {
     const name = window.prompt('请输入标签名');
     if (!name) { return window.alert('标签名不能为空'); }
-    //TODO
-    // store.createTag(name);
+    this.$store.commit('createTag',name);
   }
 }
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import clone from "@/lib/clone";
+import createId from "@/lib/createId";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        recordList: [] as RecordItem[]
+        recordList: [] as RecordItem[],
+        tagList:[] as Tag[]
     },
     mutations: {
         fetchRecords(state) {
@@ -20,6 +22,25 @@ const store = new Vuex.Store({
         },
         saveRecords(state) {
             window.localStorage.setItem('recordList', JSON.stringify(state.recordList));
+        },
+        fetchTags(state) {
+            const tagList = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+            return state.tagList = tagList;
+        },
+        createTag(state,name: string) {
+            const names = state.tagList.map(item => item.name);
+            if (names.indexOf(name) >= 0) {
+                window.alert('标签名重复了');
+                return 'duplicated';
+            }
+            const id = createId().toString();
+            state.tagList.push({id, name: name});
+            store.commit('saveTags');
+            window.alert('添加成功');
+            return 'success';
+        },
+        saveTags(state) {
+            window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
         }
     },
 });
