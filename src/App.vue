c8706c1271f4229c36b770da5112121179995012
aliyu <1324149139@qq.com>
2022-04-20 16:15:44 +0800

    重新添加了二维码的展示,并且通用移动端touch事件判断是否为手机从而展示二维码

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,85 @@
 <template>
   <div id="app">
-    <router-view/>
-    <div class="qrcode" v-show="maxWidth !== 500">
-      <img src="./assets/qrcode.png" alt="">
-    </div>
+    <router-view class="router-view"/>
+    <pop-up v-model="showQrcode" style="text-align: center">
+      <img class="qrcode" src="./assets/qrcode.png" alt="qrcode">
+      <div class="slogan">扫描二维码👆<br/>在手机上使用体验更佳✨</div>
+    </pop-up>
   </div>
 </template>
-
-<style lang="scss">
-@import "~@/assets/style/helper.scss";
-@import "~@/assets/style/reset.scss";
-
-body {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #333;
-  font-family: $font-hei;
-  font-size: 16px;
-  line-height: 1.5;
-  background: #f5f5f5;
-}
-
-#app {
-  max-width: 500px;
-  margin: 0 auto;
-}
-
-.qrcode {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
-}
-
-img {
-  max-width: 260px;
-}
-</style>
 
 <script lang="ts">
 import Vue from "vue";
 import {Component} from "vue-property-decorator";
+import PopUp from "./views/PopUp.vue";
 
-@Component
-export  default class App extends Vue{
-  maxWidth = document.documentElement.scrollWidth
+window.onload = function () {
+  document.addEventListener("touchstart", function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  }, {
+    passive: false  // 关闭被动监听
+  });
+  let lastTouchEnd = 0;
+  document.addEventListener("touchend", function (event) {
+    let now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, false);
+};
+@Component({
+  components: {PopUp}
+})
+export default class App extends Vue {
+  transitionName = "";
+  showQrcode = false;
+
+  created() {
+    if (document.documentElement.clientWidth > 500) {
+      this.showQrcode = true;
+    }
+  }
 }
 </script>
+<style lang="scss">
+@import "~@/assets/style/helper.scss";
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/animation.scss";
+
+body {
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+#app {
+  position: relative;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
+
+.router-view {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.slogan {
+  color: black;
+  font-size: larger;
+  font-weight: bolder;
+  background: lighten($color-highlight, 5%)
+
+}
+
+.qrcode {
+  max-width: 200px;
+}
+</style>
