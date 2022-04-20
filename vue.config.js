4f721e1bddddc586029360f30fea2befa88c7198
aliyu <1324149139@qq.com>
2022-04-20 14:01:18 +0800

    添加部署脚本

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 /* eslint-disable */
 const path = require('path')
 module.exports = {
-  lintOnSave: false,
-  chainWebpack:config =>{
-    const dir = path.resolve(__dirname,'src/assets/icons')
+    publicPath: process.env.NODE_ENV === 'production' ? '/mormoney/' : '/',
+    lintOnSave: false, chainWebpack: config => {
+        const dir = path.resolve(__dirname, 'src/assets/icons')
 
-    config.module
-        .rule('svg-sprite')
-        .test(/\.svg$/)
-        .include.add(dir).end() //包含icons
-        .use('svg-sprite-loader')
-        .loader('svg-sprite-loader').options({extract:false}).end()
-        .use('svgo-loader').loader('svgo-loader')
-        .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite:true}])
-    config.module.rule('svg').exclude.add(dir)  //其他 svg loader 排除  icons 目录
-  }
+        config.module
+            .rule('svg-sprite')
+            .test(/\.svg$/)
+            .include.add(dir).end() //包含icons
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader').options({extract: false}).end()
+            .use('svgo-loader').loader('svgo-loader')
+            .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+        config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+        config.module.rule('svg').exclude.add(dir)  //其他 svg loader 排除  icons 目录
+    }
 }
